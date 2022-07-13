db25dd2daf47a9e0e688175d822a132637787bb6
younglei <youngleish@gmail.com>
2022-07-13 22:52:56 +0800

    test: v1.3.3

---
+++ test/src-output/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Head />
+    <hr />
+    <Body />
+    <hr />
+    <!-- <Footer /> -->
+  </div>
+</template>
+
+<script>
+import Head from './components/head.vue';
+import Body from './components/body.vue';
+// import Footer from './components/footer';
+export default {
+  components: {
+    Head,
+    Body,
+    // Footer,
+  },
+}
+</script>
