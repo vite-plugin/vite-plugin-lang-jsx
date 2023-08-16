14cc2942db416a550947b452ecf7ff6331534f99
野鸡没名 <asd308487730@gmail.com>
2023-08-16 14:47:32 +0800

    feat: support switch AST

--- index.d.ts
+++ index.d.ts
@@ -6,5 +6,9 @@ export default langJsx;
 export interface LangJsx {
   (options?: {
     filter?: (id: string) => boolean | void;
+    /**
+     * Check JSX with ast, and use RegExp by default.
+     */
+    useAst?: boolean;
   }): Plugin[];
 }
